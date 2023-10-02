8a2c0119c5b4ff558e4d151b43efd00327898ece
Anton Lehniger <a.lehniger@ra-micro.de>
2023-10-02 10:38:49 +0200

    hmmmmmmmmm komisch ist alles wieder danke

---
+++ Frontend/rasandbox/src/components/GameBox.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="gameBox" :class="(themeClass, { 'hover-effect': enableHover })">
+    <div class="columns">
+      <div class="column is-one-third">
+        <figure class="image is-128x128">
+          <img src="https://bulma.io/images/placeholders/128x128.png" />
+        </figure>
+      </div>
+      <div class="column is-two-third gameinfo">
+        <p class="gametitle">{{ Game.name }}</p>
+        <p class="gamecompany">{{ Game.company }}</p>
+        <span class="gameprice">{{ `${Game.price.toFixed(2)}€` }} </span>
+        <p class="gamedate">{{ formattedDate }}</p>
+      </div>
+    </div>
+    <button
+      class="betterSteamButton--favorite"
+      @click="isFavorited = !isFavorited"
+    >
+      <span class="icon" v-if="Favoriteable">
+        <i :class="favGameClass" class="fa-star fa-lg" />
+      </span>
+    </button>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import moment from "moment";
+export default Vue.extend({
+  name: "GameBox",
+  props: {
+    Game: { type: Object, default: undefined },
+    enableHover: { type: Boolean, default: true },
+    Favoriteable: { type: Boolean, default: true },
+  },
+  data() {
+    return {
+      isFavorited: false,
+      formattedDate: "",
+    };
+  },
+  computed: {
+    themeClass(): string {
+      return this.$store.getters.getTheme;
+    },
+    favGameClass(): string {
+      return this.isFavorited ? "fa-solid" : "fa-regular";
+    },
+  },
+  mounted() {
+    this.formattedDate = moment(this.Game.releaseDate).format("DD.MM.YYYY");
+  },
+});
+</script>
+<style lang="scss" scoped>
+@import "../../shared/themes.scss";
+.gameBox {
+  box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.2);
+  margin: 10px 5px 0px 5px;
+  position: relative;
+  border-radius: 5px;
+  padding: 6px;
+  transition: transform 0.2s ease;
+
+  &.hover-effect:hover {
+    box-shadow: 0 5px 15px 0 rgba(0, 0, 0, 0.8);
+    transform: scale(1.08);
+    z-index: 500;
+  }
+}
+.gameprice {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  color: darkgreen;
+  font-size: 18pt;
+  font-weight: 600;
+}
+.gametitle {
+  font-size: 15pt;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+.gamecompany {
+  font-size: small;
+}
+.gameinfo {
+  position: relative;
+}
+.gamedate {
+  position: absolute;
+  bottom: 0;
+}
+.light-theme {
+  background-color: $secondary-light-theme-color;
+}
+.dark-theme {
+  background-color: $secondary-dark-theme-color;
+}
+</style>
---
+++ Frontend/rasandbox/src/components/modalBert.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="modal is-active">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+      <div class="box" :class="themeClass">
+        <slot name="main-content" />
+      </div>
+    </div>
+    <button class="close-modal-button" @click="$emit('close')">
+      <i class="fas fa-times fa-2x" />
+    </button>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "modalBert",
+  computed: {
+    themeClass() {
+      const theme = this.$store.getters.getTheme;
+      switch (theme) {
+        case "light-theme":
+          return "light-theme";
+        case "dark-theme":
+          return "dark-theme";
+        case "red-gradient-theme":
+          return "red-gradient-theme";
+        default:
+          return "light-theme";
+      }
+    },
+  },
+});
+</script>
+<style scoped>
+.modal-content {
+  width: auto;
+}
+.close-modal-button {
+  position: absolute;
+  top: 0;
+  right: 0;
+  color: white;
+  background: none;
+  border: none;
+  margin: 5px 5px;
+  cursor: pointer;
+}
+
+.close-modal-button:hover {
+  color: darkred;
+}
+</style>
